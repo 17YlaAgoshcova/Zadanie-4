daaa6321b2ab9098405b0b3fe41dbc7520442dfb
17YlaAgoshcova <ylaostrog@gmail.com>
2020-08-18 14:06:25 +0300

    Добавьте файлы проекта.

---
+++ Zadanie 4 Agoshkova/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_4_Agoshkova
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите ваш рост в сантиметрах: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int id_ves = a - 100;
+            Console.WriteLine($"Ваш идеальный вес в киллограмах: {id_ves}");
+            Console.ReadKey();
+
+        }
+    }
+}
